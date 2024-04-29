b3a8efce04ab749d14aef0f0f5986f7724b589db
socket772 <paolotto.ghilardi@gmail.com>
2024-04-29 20:40:42 +0200

    Aggiunta funzione generazione comuni

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+use std::fs::read_to_string;
+
+struct Comune {
+	codice: u16,
+	nome: String
+}
+
 fn main() {
-    println!("Hello, world!");
+
+	let comuni:Vec<Comune> = riempi_comune();
+	// Print di debug
+	// for elemento in comuni {
+	// 	println!("{}, {}", elemento.codice, elemento.nome)
+	// }
+
+	
+}
+
+fn riempi_comune() -> Vec<Comune> {
+
+	let mut comuni:Vec<Comune> = vec![];
+
+	let comune_nome_temp:Vec<String> = read_to_string("./gi_comuni_edit.txt")
+		.unwrap() // Panica se ci sono errori di lettura
+		.lines() // Dividi la stringa in un iteratore
+		.map(String::from) // Trasforma le linee in stringhe
+		.collect(); // trasforma tutte le stringhe in una collezione
+
+	let mut i:u16 = 0;
+	for citta in comune_nome_temp {
+		i = i + 1;
+		comuni.push(
+			Comune {
+				codice: i,
+				nome: citta
+		});
+	}
+
+	return comuni;
 }
